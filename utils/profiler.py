d1312f4d1a4e18bf594ce1ddba99b6f5afa923b7
TechnoDexx <itshark@mail.ru>
2023-04-28 08:10:46 +0300

    Initial Commit

--- utils/profiler.py
+++ utils/profiler.py
@@ -1,11 +1,15 @@
 import cProfile
 
 
-def profiler(label='', trace=True):
+def profiler(label='', trace=True, isClass=False):
     def OnDecorator(func):
-
         def OnCall(*args, **kwargs):
-            print('Profiling function named {0} : \n'.format(func.__name__))
+            if isClass:
+                message = 'Profiling class, named {0} :\n'
+            else:
+                message = 'Profiling function, named {0} : \n'
+
+            print(message.format(func.__name__))
             prof = cProfile.Profile()
             prof.enable()
             result = func(*args, **kwargs)
@@ -13,7 +17,9 @@ def profiler(label='', trace=True):
             if trace:
                 print('{0}{1}'.format(label, prof.print_stats()))
             return result
+
         return OnCall
+
     return OnDecorator
 
 
@@ -22,12 +28,21 @@ if __name__ == '__main__':
     def traced_function(a, b, c):
         print(a + b + c)
 
+
+    @profiler(isClass=True, label='=>')
     class A:
-        @profiler(trace=False)
+        def __init__(self, i):
+            self.i = i
+
+        def isInt(self):
+            return isinstance(self.i, int)
+
+        @profiler(label='==>')
         def traced_method(self, a, b, c):
             return print(a + b + c)
-        
+
 
     traced_function(1, 2, 3)
-    X = A()
+    X = A(0)
+    print(X.isInt())
     X.traced_method(1, 2, 3)
