97def87649968e79a8b170c1a60402a5119e229f
TechnoDexx <itshark@mail.ru>
2023-04-28 21:57:22 +0300

    Initial Commit

--- decorators/class_based_decorators.py
+++ decorators/class_based_decorators.py
@@ -2,12 +2,12 @@ class decorator:
     def __init__(self, func):
         self.func = func
 
-    def __call__(self, *args):
+    def __call__(self, *args, **kwargs):
         # args = list(*args)
         for arg in args:
             print('{0}'.format(arg))
         print('-' * 20)
-        self.func(*args)
+        self.func(*args, **kwargs)
 
 
 @decorator
